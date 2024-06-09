4870cfd8545aa4e880697172b32283d82f6e62d4
Raphael Fontoura <raphael.fontoura@gmail.com>
2024-06-09 00:13:44 -0300

    Add jdbc batch writer

--- src/main/java/com/springbatch/contasbancarias/writer/ImpressaoContaWriterConfig.java
+++ src/main/java/com/springbatch/contasbancarias/writer/ImpressaoContaWriterConfig.java
@@ -1,6 +1,15 @@
 package com.springbatch.contasbancarias.writer;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +18,24 @@ import com.springbatch.contasbancarias.dominio.Conta;
 @Configuration
 public class ImpressaoContaWriterConfig {
 	@Bean
-	public ItemWriter<Conta> impressaoContaWriter() {
-		return contas -> contas.forEach(System.out::println);
+	public JdbcBatchItemWriter<Conta> jdbcContaWriter(
+    @Qualifier("appDataSource") DataSource dataSource
+  ) {
+		return new JdbcBatchItemWriterBuilder<Conta>()
+    .dataSource(dataSource)
+    .sql("INSERT INTO conta (tipo, limite, cliente_id) VALUES (?, ?, ?)")
+    .itemPreparedStatementSetter(itemPreparedStatementSetter())
+    .build();
 	}
+
+  private ItemPreparedStatementSetter<Conta> itemPreparedStatementSetter() {
+    return new ItemPreparedStatementSetter<Conta>() {
+      @Override
+      public void setValues(Conta item, PreparedStatement ps) throws SQLException {
+        ps.setString(1, item.getTipo().name());
+        ps.setDouble(2, item.getLimite());
+        ps.setString(3, item.getClienteId());
+      }
+    };
+  }
 }
